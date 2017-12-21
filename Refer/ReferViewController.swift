0e844b759cfd2f956709dddf22387c53481faf3c
Vera <srininet@Srinis-MacBook-Air.local>
2017-12-21 18:29:18 +0530

    frist commit

---
+++ Refer/ReferViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ReferViewController.swift
+//  Refer
+//
+//  Created by Vera on 12/21/17.
+//  Copyright © 2017 Vera. All rights reserved.
+//
+
+import UIKit
+
+class ReferViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
